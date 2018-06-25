cb5291aa92bc5af15279c2443cc68c763b341796
Magdalena <m.mandat@gmail.com>
2018-06-25 15:08:10 +0100

    passed add and subtract tests with class methods

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void canAdd1And2() {
+        assertEquals(3, Calculator.add(1,2));
+    }
+
+    @Test
+    public void canSubtract4and2() {
+        assertEquals(2, Calculator.subtract(4,2));
+    }
+
+}
