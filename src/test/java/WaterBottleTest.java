f7d382a7fc5af0e9ef68061c32d7f0df363b77d8
Magdalena <m.mandat@gmail.com>
2018-06-25 15:40:03 +0100

    method and test fill added

--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -30,4 +30,10 @@ public class WaterBottleTest {
         assertEquals(0, bottle.getVolume());
     }
 
+    @Test
+    public void canFill() {
+        bottle.fill();
+        assertEquals(100, bottle.getVolume());
+    }
+
 }
